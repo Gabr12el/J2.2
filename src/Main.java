e9a80b2f23e051738a14c940588543608b011d74
Gabr12el <98735582+Gabr12el@users.noreply.github.com>
2022-03-26 13:35:55 +0400

    2правка

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         // Объявляете переменные для входных данных и параметров программы: начального счёта,
         // суммы пополнения и тп:
-        int pervo = 100;  // первоначальная сумма на счёте
-        int popol = 1600; // сумма пополнения счёта
+        int bill = 100;  // первоначальная сумма на счёте
+        int replenishment = 1600; // сумма пополнения счёта
         // Условным оператором проверяете превысила ли сумма пополнения порог и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите на экран.
-        int porog = (popol > 1000) ? 1 : 0;
-        int bonus = popol / 100 * porog;
-        int balance = pervo + popol + bonus;
+        int limit = (replenishment > 1000) ? 1 : 0;
+        int bonus = replenishment / 100 * limit;
+        int balance = bill + replenishment + bonus;
         System.out.println("Респект за лавэ!:) Твой баланс: " + balance + " руб. Твой бонус: " + bonus + " руб.");
     }
 }
